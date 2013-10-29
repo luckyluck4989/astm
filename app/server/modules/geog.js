2d7835a2af2c7716e08c5af584e9325958c39161
luckyluck4989 <luckyluck4989@gmail.com>
2013-10-29 22:41:28 +0700

    edit get country

--- app/server/modules/geog.js
+++ app/server/modules/geog.js
@@ -4,7 +4,7 @@ var cityDB  = cnMongoDB.city;
 
 // Get list country
 exports.getListCountry = function(userid,password,callback){
-	countryDB.find({}).sort({country:1}).toArray(function(err,result){
+	countryDB.find({}).sort([['country','asc']]).toArray(function(err,result){
 		if(err)
 			callback(null,'Can not get list country');
 		else
