02f17a5f68362840763a9258b809359688457c38
luckyluck4989 <luckyluck4989@gmail.com>
2013-11-07 08:26:00 +0700

    xxx

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 /**
 * MODULE DEPENDENCIES
 * -------------------------------------------------------------------------------------------------
@@ -26,7 +25,7 @@ nconf.env().file({ file: 'settings.json' });
 
 var app = express();
 app.configure(function () {
-    app.set('port', process.env.PORT || 3009);
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/app/server/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
@@ -39,19 +38,6 @@ app.configure(function () {
     app.use(app.router);
     app.use(require('less-middleware')({ src: __dirname + '/app/public' }));
     app.use(express.static(path.join(__dirname, 'app/public')));
-	/*
-	app.use(function(err, req, res, next){
-		// JSON ERROR. CODE 500
-		var jsonResult = {	func_cd: req.method.toUpperCase().replace("/",""),
-							method: req.url.toUpperCase().replace("/",""),
-							status: 500,
-							message: 'Internal Error',
-							error: err.stack,
-							result: null,
-						};
-		res.send(200,jsonResult);
-	});
-	*/
 });
 
 app.configure('development', function () {
